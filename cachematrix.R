40c7ac72a24d1ae259b5cc155d456268a5b4338d
Mike Cincotti <mikecinc28@gmail.com>
2014-11-18 20:55:16 -0500

    adding my cache matrix file to my repo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the user to calculate the inverse of a matrix
+## and cache/store it for future use rather than re-calculating it each time
 
-## Write a short comment describing this function
+## Create a list of functions that will both set and get the values of 
+## the input matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  this_inverse_matrix <- NULL
+  ## defining the "set" function. once the makeCacheMatrix is called for
+  ## for a matrix (x), this function can be used to pass in a different
+  ## matrix input parameter, with the inverse being set back to null
+  set <- function(y) {
+    x <<- y
+    this_inverse_matrix <<- NULL
+  }
+  ## defining the "get" function
+  get <- function() x
+  ## defining the "setinverse" function
+  setinverse <- function(inverse) this_inverse_matrix <<- inverse
+  ## defining the "getinverse" function
+  getinverse <- function() this_inverse_matrix
+  
+  ## defining the list of functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
 }
 
-
-## Write a short comment describing this function
+## This function will first check if the inverse of the matrix has already
+## been calculated and stored. If so, it will retrieve the inverse. If not,
+## it will calculate the inverse and then store it so that future calls
+## will not need to re-calculate.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##check to see if the inverse matrix has already been calculated
+  ##and stored
+  this_inverse_matrix <- x$getinverse()
+  ##the inverse has been found, so return it along with a message
+  if(!is.null(this_inverse_matrix)) {
+    message("getting cached data")
+    return(this_inverse_matrix)
+  }
+  data <- x$get()
+  this_inverse_matrix <- solve(data, ...)
+  x$setinverse(this_inverse_matrix)
+  this_inverse_matrix
 }
